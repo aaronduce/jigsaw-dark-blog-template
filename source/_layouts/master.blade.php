164222fda9e610e58893202a49c70e1727c8b9ba
Aaron Duce <aaronduce@lymelite.co.uk>
2020-02-17 23:57:48 +0000

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -60,7 +60,7 @@
                     &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
                 </li>
 
-                <li>
+                <li class="md:mr-2">
                     Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
                     and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
                 </li>
